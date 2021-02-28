8b8a5c326ca3d9dd4c66c7f1d5661e63bd96cadf
Sandesh <chapagainsandesh88@gmail.com>
2021-02-27 22:44:21 -0500

    add intro text

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,28 +1,28 @@
 
-const Landing = props => {
+const Landing = ({setPage}) => {
 
     return(
-        <>
-            <main role="main" className="inner cover">
-                <h1 className="cover-heading">I Design. I Build. I Deliver.</h1>
-                <p className="lead">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                    nisi ut aliquip ex ea commodo consequat. 
-                </p>
-                <p className="lead">
-                    <a className="btn btn-lg btn-secondary" href='/#'>Let's work together</a>
-                </p>
-            </main>
+        <main role="main" className="inner cover">
+            <h1 className="cover-heading">Hi there.</h1>
+            <p className="lead">
+                I am a reader. I read fiction, technical documentations, and philosophy.
 
-            <footer className="mastfoot mt-auto">
-                <div className="inner">
-                <p>Perfection is not when there is no more to add, but no more to take away. — Antoine De Saint-Exupéry
-                </p>
-                </div>
-            </footer>
-        </>
+                {/* I am a software engineer, and I love building cool stuff. 
+                Simplicity and minimalism are some of my core values, and I strive to build 
+                Elegant systems, and clean interface designs - be it APIs, or UIs - inspire me, and 
+                I strive to build products  */}
+            </p>
+            <p className="lead">
+                I have come to value simplicity and minimalism in my life, and 
+                as a software engineer, I am, quite naturally, gravitated towards building clean, elegant systems.
+
+                {/* I strive to harness the chaotic energy the world throws at me, 
+                and turn it into something crazy. */}
+            </p>
+            <p className="lead">
+                <a onClick={(e)=>{e.preventDefault();setPage("projects")}} className="btn btn-lg btn-secondary" href='/#'>Chekout my projects</a>
+            </p>
+        </main>
     )
 }
 
