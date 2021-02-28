a74dad7e5ff1cacae07b7bc8ebb7887e48df2a41
Sandesh <chapagainsandesh88@gmail.com>
2021-02-28 12:56:28 -0500

    add a contact card

---
+++ src/components/ContactCard.js
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import cafeRioImage from '../images/cafeRio.png';
+
+const ContactCard = props => {
+    return(
+        <div className="flex justify-center w-full h-1/2">
+               <div className="w-1/2 bg-blue-300 p-5">
+                   <h1>Sandesh Chapagain</h1>
+               </div> 
+        </div>
+    )
+}
+
+export default ContactCard;
