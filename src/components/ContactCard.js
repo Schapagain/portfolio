121895cb405b027a9c179c5efcb51bcfc34ecd86
Sandesh <chapagainsandesh88@gmail.com>
2021-02-28 18:50:44 -0500

    create a contacts page

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,12 +1,45 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import cafeRioImage from '../images/cafeRio.png';
+import classNames from 'classnames';
 
-const ContactCard = props => {
+const ContactCard = ({className,Ref}) => {
+    const mainClass = classNames(className,'flex flex-col justify-center w-full')
+
     return(
-        <div className="flex justify-center w-full h-1/2">
-               <div className="w-1/2 bg-blue-300 p-5">
-                   <h1>Sandesh Chapagain</h1>
-               </div> 
+        <div ref={Ref} className={mainClass}>
+            
+            <div className="w-1/3 m-auto flex flex-col p-5">
+                <div className="text-black text-center">
+                    I am always open to cool new ideas. <br/> 
+                    Feel free to reach out!
+                </div>
+                <div className="bg-purple-500 p-3 rounded-lg mt-5">
+                    <h1 className="text-2xl text-center underline p-2">Sandesh Chapagain</h1>
+                    <p className="text-sm text-center">chapagainsandesh {"{{89-1}}"} @gmail.com</p>
+                    <div className="p-3 flex justify-center text-3xl">
+                        <a 
+                        href="https://www.linkedin.com/in/schapagain/" 
+                        target="_blank" 
+                        rel="noreferrer"
+                        >
+                            <FontAwesomeIcon 
+                            className="m-1" 
+                            icon={['fab','linkedin']}
+                            />
+                        </a>
+                        <a 
+                        href="https://www.github.com/Schapagain/" 
+                        target="_blank" 
+                        rel="noreferrer"
+                        >
+                            <FontAwesomeIcon 
+                            className="m-1" 
+                            icon={['fab','github']} 
+                            />    
+                        </a>
+                    </div>
+                </div>
+                
+            </div> 
         </div>
     )
 }
