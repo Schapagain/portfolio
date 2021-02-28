ffabb893d0404d2d5eb099874b15b00efa6ff151
Sandesh <chapagainsandesh88@gmail.com>
2021-02-27 22:44:37 -0500

    change states with navbar links

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -6,21 +6,21 @@ import {
 
 } from 'reactstrap';
 
-const AppNavbar = props => {
+const AppNavbar = ({currentPage,setPage}) => {
 
   return (
-    <header class="masthead mb-auto">
+    <header className="masthead mb-auto">
         <div className="inner">
             <h3 className="masthead-brand"> Sandesh Chapagain </h3>
             <Nav className="nav-masthead justify-content-center">
                 <NavItem>
-                    <NavLink className=" nav-link active">Home</NavLink>
+                    <NavLink onClick={()=>{console.log('setting home');setPage("home")}} className={`nav-link${currentPage === "home" ? " active":""}`}>Home</NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink >Projects</NavLink>
+                    <NavLink onClick={()=>{console.log('setting pr0jects');setPage("projects")}} className={`nav-link${currentPage === "projects" ? " active":""}`}>Projects</NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink>Contact</NavLink>
+                    <NavLink className={`nav-link${currentPage === "contact" ? " active":""}`}>Contact</NavLink>
                 </NavItem>
                 
             </Nav>
