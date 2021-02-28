79393d134d65c0e9e12cdc0599db2bd6ca65cc77
Sandesh <chapagainsandesh88@gmail.com>
2021-02-27 22:44:50 -0500

    conditionally render different components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
 import Landing from './components/Landing';
 import AppNavbar from './components/AppNavbar';
+import { faCode, faCheckCircle, faMoneyCheckAlt ,faMugHot } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {useState,useEffect} from 'react';
+import Projects from './components/Projects';
+library.add(faCode,faCheckCircle,faMoneyCheckAlt,faMugHot);
+
 
 function App() {
+
+  const [page,setPage] = useState("home");
+
+  useEffect(()=>console.log(page),[page]);
+
   return (
     <div className="App">
-      <div id="cover-container" class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-        <AppNavbar />
-        <Landing />
+      <div id="cover-container" className="justify-content-center cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <AppNavbar setPage={setPage} currentPage={page}/>
+        {page === "home" && <Landing setPage={setPage}/>}
+        {page === "projects" && <Projects />}
+        <footer className="mastfoot mt-auto">
+          <div className="inner">
+          <p>Perfection is not when there is no more to add, but no more to take away. — Antoine De Saint-Exupéry
+          </p>
+          </div>
+        </footer>
       </div>
+      
     </div>
   );
 }
