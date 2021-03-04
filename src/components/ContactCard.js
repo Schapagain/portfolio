6e3e89d920ab424cb72171254a8a4a9a24eddd6d
Sandesh <chapagainsandesh88@gmail.com>
2021-03-03 23:28:18 -0500

    alter styles

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -2,17 +2,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import classNames from 'classnames';
 
 const ContactCard = ({className,Ref}) => {
-    const mainClass = classNames(className,'flex flex-col justify-center w-full')
+    const mainClass = classNames(className,'flex flex-col justify-center bg-between-the-clouds')
 
     return(
         <div ref={Ref} className={mainClass}>
-            
-            <div className="w-1/3 m-auto flex flex-col p-5">
+            <div className="w-1/2 md:w-1/3 m-auto flex flex-col p-5">
                 <div className="text-black text-center">
                     I am always open to cool new ideas. <br/> 
                     Feel free to reach out!
                 </div>
-                <div className="bg-purple-500 p-3 rounded-lg mt-5">
+                <div className="bg-blue-400 rounded-lg mt-5">
                     <h1 className="text-2xl text-center underline p-2">Sandesh Chapagain</h1>
                     <p className="text-sm text-center">chapagainsandesh {"{{89-1}}"} @gmail.com</p>
                     <div className="p-3 flex justify-center text-3xl">
