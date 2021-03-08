a30b1968af9e6755349421a15d159ca108842828
Sandesh <chapagainsandesh88@gmail.com>
2021-03-07 19:48:05 -0500

    refactor into a main container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Landing from './components/Landing';
+
 import { 
   faCode, 
   faCheckCircle, 
@@ -8,12 +8,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core';
-import ContactCard from './components/ContactCard';
-import {useRef} from 'react';
-import AppNavbar from './components/AppNavbar'
-import CafeRio from './components/CafeRio';
-import TrackIt from './components/TrackIt';
-import BlockByBlock from './components/BlockByBlock';
+import MainContainer from './components/MainContainer';
 
 library.add(
   faCode,
@@ -26,63 +21,10 @@ library.add(
   );
 
 function App() {
-    const contactRef = useRef(null);
-    const scrollToContact = () => contactRef.current.scrollIntoView({behavior:'smooth'});
-
-    const cafeRioRef = useRef(null);
-    const scrollToCafeRio = () => cafeRioRef.current.scrollIntoView({behavior:'smooth'});
-
-    const trackItRef = useRef(null);
-    const scrollToTrackIt = () => trackItRef.current.scrollIntoView({behavior:'smooth'});
-
-    const blockByBlockRef = useRef(null);
-    const scrollToBlockByBlock = () => blockByBlockRef.current.scrollIntoView({behavior:'smooth'});
-
-    const homeRef = useRef(null);
-    const scrollToHome = () => homeRef.current.scrollIntoView({behavior:'smooth'});
-
-    const projectClass = 'w-full flex flex-col p-4 h-screen bg-indigo-200 justify-between';
-    const mainClass = "w-full min-w-max-md flex flex-col justify-between mx-auto back bg-blue-200"
-
+    const mainClass = "w-full bg-theme-color min-w-max-md flex flex-col justify-between mx-auto"
   return (
     <div className={mainClass}>
-        <Landing 
-        scrollToProjects={scrollToBlockByBlock} 
-        scrollToContact={scrollToContact}
-        Ref={homeRef} 
-        className="min-h-screen z-20"/>
-        <AppNavbar 
-        scrollToContact={scrollToContact}
-        scrollToHome={scrollToHome}
-        scrollToProjects={scrollToBlockByBlock}
-        className="flex justify-between p-2 mt-0 w-full fixed z-40 top-0"/>
-        <BlockByBlock 
-        Ref={blockByBlockRef}
-        className={projectClass}
-        scrollDown={scrollToCafeRio}
-        scrollUp={scrollToHome}
-        />
-        <CafeRio 
-        Ref={cafeRioRef} 
-        className={projectClass} 
-        scrollUp={scrollToBlockByBlock}
-        scrollDown={scrollToTrackIt}
-        />
-        <TrackIt 
-        scrollUp={scrollToCafeRio}
-        scrollDown={scrollToContact}
-        Ref={trackItRef}
-        className={projectClass} />
-        <ContactCard 
-        Ref={contactRef} 
-        className="min-h-screen"
-        />
-        {/* <footer className="text-center mt-10">
-          <div className="">
-          <p>Perfection is not when there is no more to add, but no more to take away. — Antoine De Saint-Exupéry
-          </p>
-          </div>
-        </footer> */}
+        <MainContainer />
     </div>
   );
 }
---
+++ src/components/MainContainer.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import ContactCard from './ContactCard';
+import {useRef} from 'react';
+import AppNavbar from './AppNavbar'
+import CafeRio from './CafeRio';
+import TrackIt from './TrackIt';
+import BlockByBlock from './BlockByBlock';
+import Landing from './Landing';
+
+export default function MainContainer() {
+
+    const contactRef = useRef(null);
+    const scrollToContact = () => contactRef.current.scrollIntoView({behavior:'smooth'});
+
+    const cafeRioRef = useRef(null);
+    const scrollToCafeRio = () => cafeRioRef.current.scrollIntoView({behavior:'smooth'});
+
+    const trackItRef = useRef(null);
+    const scrollToTrackIt = () => trackItRef.current.scrollIntoView({behavior:'smooth'});
+
+    const blockByBlockRef = useRef(null);
+    const scrollToBlockByBlock = () => blockByBlockRef.current.scrollIntoView({behavior:'smooth'});
+
+    const homeRef = useRef(null);
+    const scrollToHome = () => homeRef.current.scrollIntoView({behavior:'smooth'});
+
+    const projectClass = 'w-full flex flex-col p-4 h-screen bg-indigo-200 justify-between';
+    return (
+        <div className="w-full mx-auto max-w-screen-2xl justify-center flex flex-col">
+            <Landing 
+            scrollToProjects={scrollToBlockByBlock} 
+            scrollToContact={scrollToContact}
+            Ref={homeRef} 
+            className="h-screen z-50"/>
+            <AppNavbar 
+            scrollToContact={scrollToContact}
+            scrollToHome={scrollToHome}
+            scrollToProjects={scrollToBlockByBlock}
+            className="flex justify-between p-2 mt-0 w-full fixed z-30 top-0"/>
+            <BlockByBlock 
+            Ref={blockByBlockRef}
+            className={projectClass}
+            scrollDown={scrollToCafeRio}
+            scrollUp={scrollToHome}
+            />
+            <CafeRio 
+            Ref={cafeRioRef} 
+            className={projectClass} 
+            scrollUp={scrollToBlockByBlock}
+            scrollDown={scrollToTrackIt}
+            />
+            <TrackIt 
+            scrollUp={scrollToCafeRio}
+            scrollDown={scrollToContact}
+            Ref={trackItRef}
+            className={projectClass} />
+            <ContactCard 
+            Ref={contactRef} 
+            className="min-h-screen"
+            />
+        </div>
+    )
+}
