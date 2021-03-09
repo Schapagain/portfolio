be6eb5a112237b438b491a4480a3d4293fb0d5c5
Sandesh <chapagainsandesh88@gmail.com>
2021-03-08 23:05:31 -0500

    redesign landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,41 +1,78 @@
 import classNames from 'classnames';
+import DeskSetup from './DeskSetup';
+import Typed from 'typed.js';
+import { useEffect } from 'react';
+import { Spring, animated } from 'react-spring/renderprops';
+import * as easings from 'd3-ease';
+
+const LandingGraphics = () => {
+    return (
+        <div className="w-1/2 flex bg-theme-color">
+            <DeskSetup className="m-auto"/>
+        </div>
+    )
+}
+
 
 const Landing = ({className,Ref,scrollToProjects,scrollToContact}) => {
 
     const mainClass = classNames(className,'flex bg-theme-color justify-center text-center');
     const buttonClass = 'hover:bg-gray-600 hover:text-white text-black bg-white p-3 ml-2 mr-2 mt-5 rounded-md';
-    const headingClass = 'text-3xl mb-2';
+    const headingClass = 'text-3xl mb-2 typed-span';
     const introClass = 'text-xl';
     const buttonGroupClass = 'flex justify-center'
     const mainTextClass = 'flex z-30 flex-col w-full md:w-1/2 p-5 justify-center';
 
+    const headOptions = {
+        startDelay: 3500,
+        strings : ["Hello there."],
+        typeSpeed: 60,
+        loop: false,
+        showCursor: false,
+        onComplete : () => startIntroText()
+    };
+
+    const startIntroText = () => {
+        new Typed('.typed-text',textOptions);
+    }
+
+    const textOptions = {
+        startDelay: 500,
+        strings : ["I build things with code."],
+        typeSpeed: 60,
+        loop: false,
+        showCursor: false
+    };
+
+    useEffect(() => {
+        let mounted = true;
+        if (mounted) {
+            new Typed('.typed-span',headOptions)
+        }
+    });
+
     return(
         <main ref={Ref} className={mainClass}>
+            <LandingGraphics />
             <div className={mainTextClass}>
-                <h1 className={headingClass}>Hi there.</h1>
+                <h1 className={headingClass}></h1>
                 <div className={introClass}>
-                    <p>I am a reader.</p> 
-                    <p>I read fiction, technical documentations, and philosophy.</p>
-                    <p>
-                        I have come to value simplicity and minimalism in my life, and 
-                        as a software engineer, I am, quite naturally, gravitated towards building clean, elegant systems.
-                    </p>
-                    <p className="mt-2">
-                        And, oh, I also love Physics.
-                    </p>
-                    {/* I am a software engineer, and I love building cool stuff. 
-                    Simplicity and minimalism are some of my core values, and I strive to build 
-                    clean interfaces. I work with both front and back end technologies,
-                    and am experienced with full-stack development.*/}
+                    <p className="typed-text"></p>
                 </div>
-                <div className={buttonGroupClass}>
-                    <a onClick={(e)=>{e.preventDefault();scrollToContact()}} className={buttonClass} href='/#'>Get in touch</a>
-                    <a onClick={(e)=>{e.preventDefault();scrollToProjects()}} className={buttonClass} href='/#'>Checkout my projects</a>
-                </div>    
+                <Spring
+                reset
+                from={{opacity:0}} 
+                to={{opacity:1}}
+                config={{delay:7500,easing:easings.easePolyInOut}}
+                >
+                    {props => (
+                    <animated.div style={props} className={buttonGroupClass}>
+                        <a onClick={(e)=>{e.preventDefault();}} className={buttonClass} href='/#'>Get in touch</a>
+                        <a onClick={(e)=>{e.preventDefault();}} className={buttonClass} href='/#'>Checkout my projects</a>
+                    </animated.div>  
+                    )}
+                </Spring>  
             </div>
-            {/* <div className={mainImageClass}>
-                <img className="" src={characterModel} alt="avatar of a persons character"/>
-            </div> */}
         </main>
     )
 }
