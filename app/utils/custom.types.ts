6599a7a60ba5be1be370e6944183eb13e96ffd71
Sandesh Chapagain <sandesh@greatbear.tech>
2023-02-06 22:49:07 -0500

    export project type

---
+++ app/utils/custom.types.ts
@@ -0,0 +1,9 @@
+export type Project = {
+  newSkills?: string[];
+  upgradedSkills?: string[];
+  title: string;
+  intro: string;
+  link?: string;
+  githubLink?: string;
+  projectImage?: string;
+};
