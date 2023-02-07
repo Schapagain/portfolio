12937a7f7d527ec0d02857c8ea31ed177ca3f074
Sandesh Chapagain <sandesh@greatbear.tech>
2023-02-06 22:48:41 -0500

    refactor

--- app/routes/projects.jsx
+++ app/routes/projects.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Navigation from "~/components/Navigation";
-import Projects from "~/components/projects/Projects";
+import Projects from "~/components/Projects";
 
 export default function Index() {
   return (
-    <div className="flex w-full flex-col justify-center items-center">
+    <div className="w-full flex flex-col items-center relative px-5 pt-24 md:pt-32 lg:pt-40">
       <h1 className="text-center text-2xl 2xl:text-title mb-10 mt-24 lg:mt-0">
         What do I do on a lazy weekend?
       </h1>
